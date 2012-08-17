20307ae38f8a31d4f98d6d262427addf905ede34
Asmmund <antony.ermolenko@gmail.com>
2012-08-17 12:54:59 +0300

    sqlite3

--- Gemfile
+++ Gemfile
@@ -5,7 +5,8 @@ gem 'rails', '3.2.6'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'mysql2'
+# gem 'mysql2'
+gem 'sqlite3'# , '~>3.0'
 
 
 # Gems used only for assets and not required
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#!/bin/env ruby
+# encoding: utf-8
 Article.delete_all
-Article.create( title: 'Title One',
-    text: 'Title One<br />Title One<br />Title One')
-
-Article.create( title: 'Title Two',
-    text: 'Title Two<br />Title Two<br />Title Two')
+Article.create(title: 'first', text: 'first')
+Article.create(title: 'second', text: 'second')
+User.delete_all
+# elmor = > S@uron
+User.create( name: 'elmor',
+password_digest: '$2a$10$yENP4/FvI/2px1ebuZsj0e0eNN11k/mmfbokOA/udwFClCPlPWe7.')
