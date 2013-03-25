0e3c26df4ac1a12fb3ba6b29f7bbd743c0a4a906
Asmmund <antony.ermolenko@gmail.com>
2013-03-25 19:19:58 +0200

    [-]removed email_spec

--- spec/requests/email_sends_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "EmailSends" do
-  xit "emails admin when article is created" do
-    @user = FactoryGirl.create(:user)
-    session[:user_id] = user.id
-    article = FactoryGirl.create(:article)
-  end
-end
